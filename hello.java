06dc671479fcd3e8c34f10cdab1f8e2c85e881ea
sachin6005 <85541761+sachin6005@users.noreply.github.com>
2022-02-10 02:24:57 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,17 @@
+package java_helloworld_example;
+
+/**
+ *
+ * @author ismailtasdelen
+ */
+public class Java_HelloWorld_Example {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+    
+}
